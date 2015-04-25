c418e497a2bd0b9aa274d18f47e7e3138d46622b
Liu Cheng <willowcheng@Lius-MacBook-Pro.local>
2015-04-24 23:51:39 -0400

    Variables and constants

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,10 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str : String = "Hello, playground"
+str = "Hello, World"
+
+let modernProgrammingLanguage = "Swift"
+
+var greeting : String = str + modernProgrammingLanguage // implicit type String
