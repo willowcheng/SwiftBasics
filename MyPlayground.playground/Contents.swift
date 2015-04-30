5601b90f37a915ced0c2c7fc25a3b3265218712d
willowcheng <e.wingus@Gmail.com>
2015-04-30 00:00:38 -0400

    Numbers and boolean

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,5 +2,10 @@
 
 import UIKit
 
-println("List:\n1\t🌔\n2\t😃\n6\tOranges")
+let language: String = "Swift"
 
+let year: Int = 2014 // Int or Integer
+
+var version: Double = 1.0 // Double = 15 decimal and Float = 7 decimal
+
+var isAwesome: Bool = true // Boolean or Bool
