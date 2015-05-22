87daec664f4bd82e749e4ef94c24f7fd3ca4cce8
willowcheng <e.wingus@Gmail.com>
2015-05-22 16:18:14 -0400

    For condition increment

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -4,14 +4,12 @@ import UIKit
 
 var todo : [String] = ["Return calls", "Write blog", "Cook dinner", "Pickup Laundry", "Buy bulbs"]
 
-var index = 0
-while index < todo.count {
+var index = 0 // index
+while index < todo.count { // condition
     println(todo[index])
-    index++
+    index++ // increment
 }
 
-index = 0
-do {
-    println(todo[index])
-    index++
-} while index < todo.count
+for var i = 0; i < todo.count; i++ {
+    println(todo[i])
+}
