9b094d43a96dc3911e2b902c3e70daffa16a1fb6
willowcheng <e.wingus@Gmail.com>
2015-05-23 12:39:46 -0400

    Comparison and logical operators

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,13 +1,24 @@
-// Conditionals
+// Comparison
 
 import UIKit
 
-let cards = 1...13
+1 == 1 // true
+2 != 1 // true
+2 > 1 // true
+1 < 2 // true
+1 >= 1 // true
+2 <= 1 // false
 
+var distance = 20 // in miles
 
-for card in cards {
-    switch card {
-    case 1, 11, 12, 13: println("Trump cards")
-    default: println(card)
-    }
+if distance < 5 {
+    println("\(distance) miles is near")
+} else if distance >= 5 && distance <= 20 {
+    println("\(distance) miles is close")
+} else {
+    println("\(distance) miles is far")
 }
+
+// && is the AND operator
+// || is the OR operator
+// ! is the NOT operator
